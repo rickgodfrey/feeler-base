5b21d46f1955d0893e8aa5b55174fb07c7e7b172
rickguo <rickguogo@outlook.com>
2020-05-16 17:00:15 +0800

    improvement

--- TFactory.php
+++ TFactory.php
@@ -10,26 +10,54 @@ namespace Feeler\Base;
 trait TFactory {
     protected static $instances;
     protected static $usingInstance;
+    protected static $usingInstanceName;
 
-    /**
-     * To Prevent The Singleton Cloning Option For Safety
-     */
-    protected function __clone(){
+    protected static function recycle($instanceName = null){
+        if(!Str::isAvailable($instanceName)){
+            static::$instances = null;
+        }
+        else{
+            if(isset(static::$instances[$instanceName])){
+                unset(static::$instances[$instanceName]);
+            }
+        }
+    }
+
+    public static function setInstance(string $instanceName, object $instance){
+        if(!Str::isAvailable($instanceName) || !is_object($instance)){
+            return false;
+        }
+
+        static::$instances[$instanceName] = $instance;
     }
 
     /**
+     * @param string $instanceName
+     * @param callable $callback
+     * @param bool $force
      * @return static()
-     * @throws \ReflectionException
      */
-    public static function &instance($instanceName = null){
+    public static function &instance(string $instanceName = "", callable $callback = null, bool $force = false){
         // if the initialization params has been changed, the singleton instance will be regenerated
-        if(Str::isAvailable($instanceName) && isset(static::$instances[$instanceName]) && is_object(static::$instances[$instanceName])){
-            static::$usingInstance = static::$instances[$instanceName];
+        if($instanceName === ""){
+            if(Str::isAvailable(static::$usingInstanceName)){
+                $instanceName = static::$usingInstanceName;
+            }
+            else if(defined(static::class."::DEFAULT_INSTANCE")){
+                $instanceName = static::DEFAULT_INSTANCE;
+            }
         }
 
-        if(!is_object(static::$usingInstance)) {
-            $reflectionObj = new \ReflectionClass(get_called_class());
-            static::$usingInstance = $reflectionObj->newInstance();
+        if(Str::isAvailable($instanceName) && isset(static::$instances[$instanceName]) && is_object(static::$instances[$instanceName])){
+            static::$usingInstance = static::$instances[$instanceName];
+            static::$usingInstanceName = $instanceName;
+        }
+
+        if(!is_object(static::$usingInstance) || $force) {
+            if(is_callable($callback)){
+                static::$usingInstance = call_user_func($callback);
+                static::$usingInstanceName = $instanceName;
+            }
         }
 
         return static::$usingInstance;
