da8c184dc3c031a0c1119effa50d69d52bdf3104
rickguo <rickguogo@outlook.com>
2020-05-17 13:42:59 +0800

    improvement

--- Str.php
+++ Str.php
@@ -173,7 +173,7 @@ class Str extends BaseClass {
             return $case === self::CASE_UPPER ? strtoupper($char) : strtolower($char);
         }
 
-        return null;
+        return "";
     }
 
     public static function getLastLetter(string $string, $case = self::CASE_UPPER): string{
@@ -183,7 +183,7 @@ class Str extends BaseClass {
             return $case === self::CASE_UPPER ? strtoupper($char) : strtolower($char);
         }
 
-        return null;
+        return "";
     }
 
     public static function isZhChar(string $string): bool{
@@ -214,7 +214,7 @@ class Str extends BaseClass {
                 return $case === self::CASE_UPPER ? strtoupper($letter) : $letter;
             }
 
-            return null;
+            return "";
         }
 
         $char = self::getChar($string, $posititon);
@@ -279,7 +279,7 @@ class Str extends BaseClass {
         }
         else
         {
-            return null;
+            return "";
         }
     }
 
@@ -289,5 +289,81 @@ class Str extends BaseClass {
 
     public static function getZhLastLetter(string $string, $case = self::CASE_UPPER): string{
         return self::getZhLetter($string, -1, $case);
+    }
+
+    public static function hideParts($string, $hideRate = 0.5, $symbol = "*", $symbolRepeatTimes = 4){
+        if(!self::isAvailable($string) || !is_float($hideRate) || $hideRate <= 0){
+            return "";
+        }
+
+        if($hideRate >= 1){
+            return $string;
+        }
+
+        $strLen = mb_strlen($string);
+
+        if($strLen == 1){
+            return $symbol;
+        }
+
+        $hideLen = ceil($strLen * $hideRate);
+        $showLen = $strLen - $hideLen;
+
+        if($symbolRepeatTimes == "AUTO"){
+            $symbolRepeatTimes = $hideLen;
+        }
+        else if(!Number::isInteric($symbolRepeatTimes)){
+            $symbolRepeatTimes = 4;
+        }
+
+        if($symbolRepeatTimes > $hideLen){
+            $symbolRepeatTimes = $hideLen;
+        }
+
+        if($symbolRepeatTimes < 1){
+            $symbolRepeatTimes = 1;
+        }
+
+        if($strLen == 2){
+            $leftPlace = 0;
+            $rightPlace = 1;
+        }
+        else{
+            $leftPlace = ceil($showLen / 2);
+            $rightPlace = $showLen - $leftPlace;
+        }
+
+        $stringParts = [
+            mb_substr($string, 0, $leftPlace),
+            mb_substr($string, $leftPlace, $hideLen),
+            mb_substr($string, ($leftPlace + $hideLen))
+        ];
+
+        return $stringParts[0].str_repeat($symbol, $symbolRepeatTimes).$stringParts[2];
+    }
+
+    public static function utf8Encode($string){
+        if(!self::isAvailable($string)){
+            return "";
+        }
+
+        $string = utf8_encode($string);
+        $string = utf8_decode($string);
+
+        return $string;
+    }
+
+    public static function mbSplit($string, $len = 1) {
+        $start = 0;
+        $strlen = mb_strlen($string);
+        $array = [];
+
+        while ($strlen){
+            $array[] = mb_substr($string, $start, $len,"utf8");
+            $string = mb_substr($string, $len, $strlen,"utf8");
+            $strlen = mb_strlen($string);
+        }
+
+        return $array;
     }
 }
