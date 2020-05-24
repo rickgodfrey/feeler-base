1ebc03062b44f396b3cf2b8685d5c96d07aaaa9f
rickguo <rickguogo@outlook.com>
2020-05-25 05:06:29 +0800

    improvement

--- Str.php
+++ Str.php
@@ -21,7 +21,14 @@ class Str extends BaseClass {
 
     const REPEAT_TIMES_AUTO = "REPEAT_TIMES_AUTO";
 
+    const SUPPORTED_ENCODINGS = ["UTF-8", "ASCII", "UTF-32", "UTF-32BE", "UTF-32LE", "UTF-16", "UTF-16BE", "UTF-16LE", "UTF-7", "UTF7-IMAP", "UTF-8-Mobile", "UCS-4", "UCS-4BE", "UCS-4LE", "UCS-2", "UCS-2BE", "UCS-2LE", "GB18030", "KOI8-R", "KOI8-U", "EUC-CN", "EUC-KR", "EUC-JP", "EUC-TW", "eucJP-win", "SJIS", "SJIS-win", "SJIS-mac", "SJIS-Mobile", "JIS", "JIS-ms", "CP866", "CP932", "CP936", "CP950", "CP51932", "CP50220", "CP50220raw", "CP50221", "CP50222", "ISO-8859-1", "ISO-8859-2", "ISO-8859-3", "ISO-8859-4", "ISO-8859-5", "ISO-8859-6", "ISO-8859-7", "ISO-8859-8", "ISO-8859-9", "ISO-8859-10", "ISO-8859-13", "ISO-8859-14", "ISO-8859-15", "ISO-8859-16", "ISO-2022-KR", "ISO-2022-JP", "ISO-2022-JP-MS", "ISO-2022-JP-MOBILE", "byte2be", "byte2le", "byte4be", "byte4le", "7bit", "8bit", "BASE64", "HTML-ENTITIES", "HZ", "BIG-5", "UHC", "Windows-1251", "Windows-1252", "ArmSCII-8"];
     const CASE_MINUSES = [0, 538976288, 2105376, 8224, 32, 536870944, 536870912, 538968064, 538976256, 2097152, 2105344, 8192];
+    const UPPER_LETTERS_DICT = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    const LOWER_LETTERS_DICT = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+    public static function isString($string): bool{
+        return is_string($string);
+    }
 
     public static function isAvailable(&$string) : bool{
         if(!is_string($string)){
@@ -37,109 +44,40 @@ class Str extends BaseClass {
         return true;
     }
 
-    public static function isString($string): bool{
-        return is_string($string);
-    }
-
     public static function supportedEncodings(): array{
-        return [
-            "UTF-8",
-            "ASCII",
-            "UTF-32",
-            "UTF-32BE",
-            "UTF-32LE",
-            "UTF-16",
-            "UTF-16BE",
-            "UTF-16LE",
-            "UTF-7",
-            "UTF7-IMAP",
-            "UTF-8-Mobile",
-            "UCS-4",
-            "UCS-4BE",
-            "UCS-4LE",
-            "UCS-2",
-            "UCS-2BE",
-            "UCS-2LE",
-            "GB18030",
-            "KOI8-R",
-            "KOI8-U",
-            "EUC-CN",
-            "EUC-KR",
-            "EUC-JP",
-            "EUC-TW",
-            "eucJP-win",
-            "SJIS",
-            "SJIS-win",
-            "SJIS-mac",
-            "SJIS-Mobile",
-            "JIS",
-            "JIS-ms",
-            "CP866",
-            "CP932",
-            "CP936",
-            "CP950",
-            "CP51932",
-            "CP50220",
-            "CP50220raw",
-            "CP50221",
-            "CP50222",
-            "ISO-8859-1",
-            "ISO-8859-2",
-            "ISO-8859-3",
-            "ISO-8859-4",
-            "ISO-8859-5",
-            "ISO-8859-6",
-            "ISO-8859-7",
-            "ISO-8859-8",
-            "ISO-8859-9",
-            "ISO-8859-10",
-            "ISO-8859-13",
-            "ISO-8859-14",
-            "ISO-8859-15",
-            "ISO-8859-16",
-            "ISO-2022-KR",
-            "ISO-2022-JP",
-            "ISO-2022-JP-MS",
-            "ISO-2022-JP-MOBILE",
-            "byte2be",
-            "byte2le",
-            "byte4be",
-            "byte4le",
-            "7bit",
-            "8bit",
-            "BASE64",
-            "HTML-ENTITIES",
-            "HZ",
-            "BIG-5",
-            "UHC",
-            "Windows-1251",
-            "Windows-1252",
-            "ArmSCII-8",
-        ];
+        return self::SUPPORTED_ENCODINGS;
     }
 
     public static function upperLettersDict(): array{
-        return ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+        return self::UPPER_LETTERS_DICT;
     }
 
     public static function lowerLettersDict(): array{
-        return ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+        return self::LOWER_LETTERS_DICT;
     }
 
     public static function isLetter(string $string): bool{
-        return preg_match("/[a-z]/i", $string);
+        return preg_match("/[a-zA-Z]/", $string);
+    }
+
+    public static function isLetters(string $string): bool{
+        return preg_match("/[a-zA-Z]+/", $string);
     }
 
     public static function isUpperLetter(string $string): bool{
         return preg_match("/[A-Z]/", $string);
     }
 
+    public static function isUpperLetters(string $string): bool{
+        return preg_match("/[A-Z]+/", $string);
+    }
+
     public static function isLowerLetter(string $string): bool{
         return preg_match("/[a-z]/", $string);
     }
 
-    public static function isPureLetters(string $string): bool{
-        return preg_match("/[a-z\s]+/i", $string);
+    public static function isLowerLetters(string $string): bool{
+        return preg_match("/[a-z]+/", $string);
     }
 
     public static function detectEncoding(string $string): string{
@@ -156,7 +94,7 @@ class Str extends BaseClass {
         return mb_substr($string, $position, 1);
     }
 
-    public static function cutOut(string $string, int $position = 0, int $length = -1): string{
+    public static function slice(string $string, int $position = 0, int $length = -1): string{
         if(!self::isAvailable($string) || $length < -1 || $length === 0){
             return false;
         }
@@ -177,20 +115,32 @@ class Str extends BaseClass {
     }
 
     public static function getFirstLetter(string $string, $case = self::CASE_UPPER): string{
-        $char = self::getFirstChar($string);
+        if(!($char = self::getFirstChar($string))){
+            return "";
+        }
 
         if(self::isLetter($char)){
-            return $case === self::CASE_UPPER ? strtoupper($char) : strtolower($char);
+            return ($case === self::CASE_UPPER) ? strtoupper($char) : strtolower($char);
+        }
+
+        if(self::isZhChar($char)){
+            return ($case === self::CASE_UPPER) ? strtoupper(self::getZhLetter($char)) : strtolower(self::getZhLetter($char));
         }
 
         return "";
     }
 
     public static function getLastLetter(string $string, $case = self::CASE_UPPER): string{
-        $char = self::getLastChar($string);
+        if(!($char = self::getLastChar($string))){
+            return "";
+        }
 
         if(self::isLetter($char)){
-            return $case === self::CASE_UPPER ? strtoupper($char) : strtolower($char);
+            return ($case === self::CASE_UPPER) ? strtoupper($char) : strtolower($char);
+        }
+
+        if(self::isZhChar($char)){
+            return ($case === self::CASE_UPPER) ? strtoupper(self::getZhLetter($char)) : strtolower(self::getZhLetter($char));
         }
 
         return "";
@@ -215,11 +165,11 @@ class Str extends BaseClass {
 
     public static function getZhLetter(string $string, int $posititon = 0, string $case = self::CASE_UPPER): string{
         if(!self::isAvailable($string)){
-            return false;
+            return "";
         }
 
         if(self::detectEncoding($string) === self::ENCODING_ASCII){
-            $letter = ($letter = self::getChar($string, $posititon)) && self::isLetter($letter) ? $letter : false;
+            $letter = ($letter = self::getChar($string, $posititon)) && self::isLetters($letter) ? $letter : false;
             if($letter){
                 return $case === self::CASE_UPPER ? strtoupper($letter) : $letter;
             }
@@ -229,7 +179,7 @@ class Str extends BaseClass {
 
         $char = self::getChar($string, $posititon);
         if(!$char){
-            return false;
+            return "";
         }
 
         $char = iconv(self::ENCODING_UTF8,"gb2312", $char);
@@ -350,67 +300,6 @@ class Str extends BaseClass {
         ];
 
         return $stringParts[0].str_repeat($symbol, $symbolRepeatTimes).$stringParts[2];
-    }
-
-    public static function utf8Encode($string){
-        if(!self::isAvailable($string)){
-            return "";
-        }
-
-        $string = utf8_encode($string);
-        $string = utf8_decode($string);
-
-        return $string;
-    }
-
-    public static function mbSlice($string, $len = 1) {
-        $start = 0;
-        $strlen = mb_strlen($string);
-        $array = [];
-
-        while ($strlen){
-            $array[] = mb_substr($string, $start, $len, "utf8");
-            $string = mb_substr($string, $len, $strlen, "utf8");
-            $strlen = mb_strlen($string);
-        }
-
-        return $array;
-    }
-
-    public static function isUpperCase($string) : bool{
-        if(!self::isAvailable($string)){
-            return false;
-        }
-
-        if($string !== strtoupper($string)){
-            return false;
-        }
-
-        return true;
-    }
-
-    public static function isLowerCase($string) : bool{
-        if(!self::isAvailable($string)){
-            return false;
-        }
-
-        if($string !== strtolower($string)){
-            return false;
-        }
-
-        return true;
-    }
-
-    public static function isMixedCase($string) : bool{
-        if(!self::isAvailable($string)){
-            return false;
-        }
-
-        if(!self::isLowerCase($string) && !self::isUpperCase($string)){
-            return false;
-        }
-
-        return true;
     }
 
     public static function lettersAggr($string) : array{
